fc251925a776985b0d9e6a0a2cf0b84696a4363c
P3pS1Sup3r <60550100+P3pS1Sup3r@users.noreply.github.com>
2020-02-21 10:25:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-wwindow.onload = function() {
+window.onload = function() {
 	
 	var time_list = [
-	"2020-02-05T03:19:14.670028+00:00",
+	"2020-02-05T03:21:14.670028+00:00",
 	"2020-02-05T12:19:14.662459+01:00",
 	"2020-02-05T13:19:14.654707+02:00",
 	"2020-02-05T04:19:14.654578+00:00",
@@ -11,7 +11,7 @@ wwindow.onload = function() {
 	"2020-02-05T13:19:14.921953+02:00",
 	"2020-02-05T12:19:14.984199+01:00",
 	"2020-02-05T14:19:14.987572+03:00",
-	"2020-02-05T4:20:15.016105+02:00",
+	"2020-02-05T04:20:15.016105+02:00",
 	"2020-02-05T12:19:15.009760+01:00",
 	"2020-02-05T13:19:15.063263+02:00",
 	"2020-02-05T11:19:15.080940+00:00",
@@ -23,6 +23,8 @@ wwindow.onload = function() {
 	var soon_date_list = [];
 	
 	var N_date_list = [];
+	
+	var need_timezone;
 	
 	/*var reqestJson = "http://worldtimeapi.org/api/timezone/America/Argentina/Salta";
 	var reqest = new XMLHttpRequest();
@@ -40,6 +42,7 @@ wwindow.onload = function() {
 	//console.log (time_list);
 	
 	Time_to_Data();
+	console.log(date_list);
 	Close_time();
 	
 	console.log ("420");
@@ -47,11 +50,16 @@ wwindow.onload = function() {
 	console.log ("soon");
 	console.log (soon_date_list);
 	
+	Calculation_UTP ();
+	
+	
+	//console.log(timezones);
+	
 	function Time_to_Data() {
 		time_list.forEach(cur => Data_push (cur));
 		
 		function Data_push (el) {
-			datt = new Date (el.substring(0,19));
+			datt = new Date (el.substr(0,16));
 			date_list.push(datt);
 		}
 	}
@@ -79,11 +87,33 @@ wwindow.onload = function() {
 		date_list.forEach (cur => Calculation_Time(cur));
 		
 		function Calculation_Time (el) {
-			if (date_list[el].getHours() == Foru_twenty.getHours() & date_list[el].getMinutes() == Foru_twenty.getMinutes())
-				N_date_list.push(date_list[el]);
+			console.log(el.getHours());
+			if (el.getHours() == Foru_twenty.getHours() & el.getMinutes() == Foru_twenty.getMinutes())
+				N_date_list.push(el);
 			else
-			if (date_list[el].getHours() == Foru_twenty.getHours())
-				soon_date_list.push(date_list[el]);
+			if ((el.getHours() == 4 & el.getMinutes()<20) | (el.getHours() == 3 & el.getMinutes() > 20))
+				soon_date_list.push(el);
 		}
 	}
+
+	function Calculation_UTP () {
+		
+		var reqestJson = "http://worldtimeapi.org/api/timezone/africa/abidjan";
+		var reqest = new XMLHttpRequest();
+		reqest.open('GET', reqestJson);
+		reqest.responseType = 'json';
+		reqest.send();	
+		
+		reqest.onload = function() {
+			var abidjan_time_s = reqest.response;
+			var abidjan_time = new Date (abidjan_time_s['datetime'].substr(0,16));
+			need_timezone = 4-abidjan_time.getHours();
+			console.log(need_timezone);
+			timezones.forEach (function(value,key) {
+				if (String(value)==String(need_timezone)) console.log(key);
+			});
+		}
+	}	
+	
+	
 }
